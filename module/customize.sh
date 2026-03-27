2266df99039e0c4b666a4f37b632305ce52a5ff0
Tools-app <localhost.hutao@gmail.com>
2026-03-28 07:12:44 +0800

    module: fix error handler

--- module/customize.sh
+++ module/customize.sh
@@ -4,7 +4,7 @@ if [ -z "$APATCH" ] && [ -z "$KSU" ]; then
   abort "! unsupported root platform"
 fi
 
-if [ -z "$KSU_LATE_LOAD" ]; then
+if [ -n "${KSU_LATE_LOAD:-}" ]; then
   abort "! unsupported late load mode"
 fi
 
